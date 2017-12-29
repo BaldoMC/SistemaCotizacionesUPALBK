d7b4dcf41878afc60945fe6da0d34b1539110b08
Kevin Araya Reygada <kevxar@gmail.com>
2017-12-29 15:47:46 -0300

    Arreglo de jerarquía de archivos

---
+++ Models/Cotizacion.cs
@@ -0,0 +1,29 @@
+using System;
+
+/// <summary>
+/// Archivo donde se definen las clases del Dominio del problema.
+/// </summary>
+namespace Cotizaciones.Models
+{
+
+    /// <summary>
+    /// Clase que representa a una cotizacion en el Sistema.
+    /// </summary>
+    /// <remarks>
+    /// Esta clase pertenece al modelo del Dominio y posee las siguientes restricciones:
+    /// - No permite valores null en sus atributos.
+    /// </remarks>
+    public class Cotizacion
+    {
+        public int ID { get; set; }
+        public string nombreCliente{get ; set; }
+        public string nombreProductor{get ; set; }
+        public DateTime fechaCreacion {get; set; }
+        public DateTime fechaValidez{get; set; }
+        public string descripcion{get; set; }
+        public int costo{get; set; }
+        public int total {get; set; }
+        public string estado{get; set; }
+
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# SistemaCotizacionesUPALBK
-Proyecto de Sistema de Cotizaciones para UPA, por parte del grupo LBK del curso Ingeniería de Software I de la UCN.
