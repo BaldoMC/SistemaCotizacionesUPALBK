b6d1b2489683fc2f056c8af34a6d3479a59266e4
Kevin Araya Reygada <kevxar@gmail.com>
2017-12-30 15:49:44 -0300

    Revert "Creación de Services"
    This reverts commit 03df816db6221013b2b146d4e96b17f59f083190.

--- Data/CotizacionesContext.cs
+++ Data/CotizacionesContext.cs
@@ -4,16 +4,9 @@ using Cotizaciones.Models;
 namespace Cotizaciones.Data{
     public class CotizacionesContext : DbContext
     {
-        internal object cotizaciones;
-
         public CotizacionesContext(DbContextOptions<CotizacionesContext> options) : base(options)
         {
         }
-
-        public CotizacionesContext()
-        {
-        }
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //Utilizacion de SQLite como backend
@@ -22,5 +15,11 @@ namespace Cotizaciones.Data{
         public DbSet<Persona> Personas {get; set;}
 
         public DbSet<Cotizacion> Cotizaciones {get;set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Persona>().HasKey(x => x.Rut);
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
